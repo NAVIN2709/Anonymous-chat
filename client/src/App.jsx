29962a05def30175835ae2c2fbb596eca4a5e4fa
Naveen <naveen@Naveens-MacBook-Pro.local>
2025-03-29 01:34:30 +0530

    dom issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx"; // Make sure this file exists
 
 const App = () => {
   return (
     <div className="App bg-[#000080] min-h-screen">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-        </Routes>
-      </Router>
+      <Home />
     </div>
   );
 };
